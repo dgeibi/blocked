83dd157b3a456848a68c5141f5598a8e901742fe
dgeibi <pnginside@gmail.com>
2016-04-04 21:11:03 +0800

    minor modify

--- generate.sh
+++ generate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 ./updater.py -s
-./bricks makpac 'PROXY 127.0.0.1:1080'
+
+if [ -z "$1" ]; then
+    ./bricks makpac 'PROXY 127.0.0.1:1080'
+else
+    ./bricks makpac "$1"
+fi
+
 ./make-dnsmasq-ipset.sh
