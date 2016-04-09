5da824bbf17f944bfd254c501d9d668286fe4662
dgeibi <png.inside@gmail.com>
2016-04-09 20:11:46 +0800

    remove src of wongsyrone

--- import-from-original.sh
+++ import-from-original.sh
@@ -3,7 +3,7 @@
 Output="./original.txt"
 
 curl -o $Output https://raw.githubusercontent.com/Leask/BRICKS/master/gfw.bricks
-curl https://raw.githubusercontent.com/wongsyrone/domain-block-list/master/domains.txt >> $Output
+#curl https://raw.githubusercontent.com/wongsyrone/domain-block-list/master/domains.txt >> $Output
 
 cat $Output | xargs ./updater.py -a
 
